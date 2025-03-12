351079037fbbe3f654a8ed95d50afde4ce90aa3a
fabiquevedo94 <90694120+fabiquevedo94@users.noreply.github.com>
2025-03-12 09:58:14 -0500

    Add files via upload

---
+++ SistemaPago3/src/com/sistemapago/service/Service.java
@@ -0,0 +1,17 @@
+package com.sistemapago.service;
+
+import com.sistemapago.model.Pago;
+
+public class Service {
+    public boolean aplicarPagoTC(Pago pago) {
+        return true;
+    }
+
+    public boolean aplicarPagoPayPal(Pago pago) {
+        return true;
+    }
+
+    public boolean aplicarPagoNuevoMetodo(Pago pago) {
+        return true;
+    }
+}
